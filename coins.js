05c38e988dbdee5a3262f9129738d918cbe3c40e
Ben Conner <ben.conner40@gmail.com>
2016-11-29 15:27:43 -0600

    added function to process dollar change

--- coins.js
+++ coins.js
@@ -9,15 +9,73 @@
   }
 
 */
+var quarter = 0;
+var dime = 0;
+var nickel = 0;
+var pennie = 0;
+var subCoin = 0;
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
+function change(dollerAmount){
+    if(dollerAmount >= .01){
+        for( ; dollerAmount > 0; dollerAmount -= subCoin){
+            if(dollerAmount > .25 ){
+                quarter += 1;
+                subCoin = .25;
+            }else if(dollerAmount > .10){
+                dime += 1;
+                subCoin = .10;
+            }else if(dollerAmount > .05){
+                nickel += 1;
+                subCoin = .05;
+            }else if(dollerAmount > .01) {
+                pennie += 1;
+                subCoin = .01;
+            }else{
+                console.log("quarters = " + quarter + " dimes = " + dime + " nickels = " + nickel + " pennies = " + pennie);
+            }
 
-  coinPurse.quarters = 0;
-
-  return coinPurse;
+        }
+        
+    }  
 }
 
-var coins = coinCounter()
-console.log();
+var askForMoney = prompt("Doller Amount");
+console.log(askForMoney)
+// askForMoney *= 100;
+change(askForMoney);
+
+// function coinCounter (dollerAmount) {
+//     var quarter = 0;
+//     var dime = 0;
+//     var nickel = 0;
+//     var pennie = 0;
+//   // Initialize a JavaScript object to hold the coins
+//     var coinPurse = {"quarters", "dimes", "nickels", "pennies"};
+//     do{
+//         if(dollerAmount >= 25 ){
+//             quarter += 1;
+//             subCoin -= 25;
+//             console.log(quarter);
+//         }else if(dollerAmount >= 10){
+//             dime += 1;
+//             subCoin = 10;
+//             console.log(dime);
+//         }else if(dollerAmount >= 5){
+//             nickel += 1;
+//             subCoin = 5;
+//         }else{
+//             pennies += 1;
+//             subCoin = 1;
+//         }
+//     }while(dollerAmount)
+
+//     coinPurse.quarters = 0;
+
+//     return coinPurse;
+    
+// }        
+
+// var coins = coinCounter()
+// console.log();
+
+
